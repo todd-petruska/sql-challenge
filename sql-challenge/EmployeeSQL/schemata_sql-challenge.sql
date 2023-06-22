5166e157444571253cbcffdade818ccf5c58c83c
todd-petruska <128247739+todd-petruska@users.noreply.github.com>
2023-06-22 17:17:39 -0400

    Update schemata_sql-challenge.sql

--- sql-challenge/EmployeeSQL/schemata_sql-challenge.sql
+++ sql-challenge/EmployeeSQL/schemata_sql-challenge.sql
@@ -10,7 +10,7 @@ DROP TABLE IF EXISTS titles;
 
 --Create a table for employees
 CREATE TABLE "employees" (
-    "emp_no" int NOT NULL,
+    "emp_no" VARCHAR(20) NOT NULL,
     "emp_title_id" VARCHAR(5) NOT NULL,
     "birth_date" date NOT NULL,
     "first_name" VARCHAR(20) NOT NULL,
@@ -24,22 +24,22 @@ CREATE TABLE "employees" (
 
 --Create a table for salaries
 CREATE TABLE "salaries" (
-    "emp_no" int NOT NULL,
+    "emp_no" VARCHAR(20) NOT NULL,
     "salary" int NOT NULL
 );
 
 --Create a table for department manager
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(4) NOT NULL,
-    "emp_no" int NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+    "emp_no" VARCHAR(20) NOT NULL,
+    CONSTRAINT "fk_dept_manager" PRIMARY KEY (
         "emp_no"
      )
 );
 
 --Create table for departments associated with the employees
 CREATE TABLE "dept_emp" (
-    "emp_no" int NOT NULL,
+    "emp_no" VARCHAR(20) NOT NULL,
     "dept_no" VARCHAR(20) NOT NULL
 );
 
@@ -56,23 +56,9 @@ CREATE TABLE "departments" (
 );
 
 --Alter the table and add contraints for the Primary and Foreign Keys
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "dept_emp" ("emp_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
 
 -- Import data for each csv file
 -- View the table and ensure all data is imported correctly
