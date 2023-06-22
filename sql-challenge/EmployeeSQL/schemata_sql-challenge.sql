d4ae049c5475ffcca29db51907f405727d825278
todd-petruska <128247739+todd-petruska@users.noreply.github.com>
2023-06-22 18:10:55 -0400

    Update schemata_sql-challenge.sql

--- sql-challenge/EmployeeSQL/schemata_sql-challenge.sql
+++ sql-challenge/EmployeeSQL/schemata_sql-challenge.sql
@@ -25,7 +25,9 @@ CREATE TABLE "employees" (
 --Create a table for salaries
 CREATE TABLE "salaries" (
     "emp_no" VARCHAR(20) NOT NULL,
-    "salary" int NOT NULL
+    "salary" int NOT NULL,
+	CONSTRAINT "pk_salaries" PRIMARY KEY (
+		"emp_no")
 );
 
 --Create a table for department manager
@@ -33,8 +35,7 @@ CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(4) NOT NULL,
     "emp_no" VARCHAR(20) NOT NULL,
     CONSTRAINT "fk_dept_manager" PRIMARY KEY (
-        "emp_no"
-     )
+        "emp_no")
 );
 
 --Create table for departments associated with the employees
@@ -46,19 +47,24 @@ CREATE TABLE "dept_emp" (
 --Create a table for titles
 CREATE TABLE "titles" (
     "title_id" VARCHAR(5) NOT NULL,
-    "title" VARCHAR(20) NOT NULL
+    "title" VARCHAR(30) NOT NULL,
+	CONSTRAINT "pk_titles" PRIMARY KEY (
+		"title_id"
+	)
 );
 
 --Create a table for departments
 CREATE TABLE "departments" (
     "dept_no" VARCHAR(4) NOT NULL,
-    "dept_name" VARCHAR(20) NOT NULL
+    "dept_name" VARCHAR(20) NOT NULL,
+	CONSTRAINT "pk_departments" PRIMARY KEY (
+		"dept_no"
+	)
 );
 
 --Alter the table and add contraints for the Primary and Foreign Keys
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
-
 
 -- Import data for each csv file
 -- View the table and ensure all data is imported correctly
